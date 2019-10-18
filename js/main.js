d2164e35f136ad10a14d798b7dbec4652c431a19
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-18 12:06:56 -0400

    Javascript accordion setup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-(function accordion( ) {
+// API Template
+// const starWarsApi = 'https://swapi.co/api/';
+// fetch(starWarsApi)
+//     .then(res => res.json())
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .catch(err => {
+//         console.log("something went wrong...", err)
+//     })
+
+function createAccordion() {
+    for(let i = 0; i < 10; i++) {
+        const content = document.querySelector('.content');
+        let button = document.createElement('button');
+        button.classList.add('accordion')
+        button.innerText = ('This will be a name')
+        content.appendChild(button);
+        let div = document.createElement('div');
+        div.classList.add('panel');
+        button.after(div)
+        let p = document.createElement('p');
+        p.innerText = ('This will be content')
+        div.appendChild(p);
+    }
+    accordion();
+}
+createAccordion();
+
+function accordion() {
     const accordion = document.getElementsByClassName("accordion");
     for (let i = 0; i < accordion.length; i++) {
         accordion[i].addEventListener("click", function () {
@@ -11,21 +40,4 @@
             }
         });
     }
-}());
-
-// API Template
-// const starWarsApi = 'https://swapi.co/api/';
-// fetch(starWarsApi)
-//     .then(res => res.json())
-//     .then(res => {
-//         console.log(res)
-//     })
-//     .catch(err => {
-//         console.log("something went wrong...", err)
-//     })
-
-const content = document.querySelector('.content');
-let button = document.createElement('button');
-button.classList.add('accordion')
-button.innerText = ('This is a test')
-content.appendChild(button);
+}
